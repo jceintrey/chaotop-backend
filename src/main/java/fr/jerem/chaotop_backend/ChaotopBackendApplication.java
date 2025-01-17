e428b3f326bac06d47d5fb6d690cb9681f28120a
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-01-17 09:25:54 +0100

    Initial Commit

---
+++ src/main/java/fr/jerem/chaotop_backend/ChaotopBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.jerem.chaotop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaotopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaotopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/jerem/chaotop_backend/ChaotopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jerem.chaotop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaotopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
