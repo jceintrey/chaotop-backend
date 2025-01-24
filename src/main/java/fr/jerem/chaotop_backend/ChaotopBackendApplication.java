cc456176efa03c9515b1bcd0079d35dc20e21b66
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-01-24 14:41:59 +0100

    hide-secrets : Configuration des secrets à partir des variables d'environnement  - AppConfigProperties: la classe est utilisée pour gérer les variables    personnalisée de l'application

--- src/main/java/fr/jerem/chaotop_backend/ChaotopBackendApplication.java
+++ src/main/java/fr/jerem/chaotop_backend/ChaotopBackendApplication.java
@@ -2,8 +2,12 @@ package fr.jerem.chaotop_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import fr.jerem.chaotop_backend.configuration.properties.AppConfigProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppConfigProperties.class)
 public class ChaotopBackendApplication {
 
 	public static void main(String[] args) {
