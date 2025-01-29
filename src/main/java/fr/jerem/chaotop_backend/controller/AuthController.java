122df66edb8e027a486be83d035f6e3077d10027
Jérémie Ceintrey <jeremie.ceintrey@gmail.com>
2025-01-29 18:36:15 +0100

    remove unused imports

--- src/main/java/fr/jerem/chaotop_backend/controller/AuthController.java
+++ src/main/java/fr/jerem/chaotop_backend/controller/AuthController.java
@@ -5,8 +5,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
